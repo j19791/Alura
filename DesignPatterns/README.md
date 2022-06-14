b105a6b07cc751bf95eeac18e312bb488b1558bc
Jefferson <jeffrm@gmail.com>
2022-06-13 21:31:21 -0300

    3. Calculando Impostos

--- DesignPatterns/README.md
+++ DesignPatterns/README.md
@@ -1,1 +1,8 @@
-##Anotações do curso de Design Pattern
+## Anotações do curso de Design Pattern
+
+- Padrões de projeto
+- OO
+- problemas repetitivos - soluções prontas
+
+### Strategy
+
---
+++ DesignPatterns/src/br/com/alura/loja/TestesImpostos.java
@@ -0,0 +1,18 @@
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class TestesImpostos {
+
+	public static void main(String[] args) {
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+		CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
+		System.out.println(calculadoraDeImpostos.calcular(orcamento, TipoImposto.ISS));
+
+	}
+
+}
