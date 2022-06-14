0bcc03ca338e02a0b404153c8c90c2db36406731
Jefferson <jeffrm@gmail.com>
2022-06-13 22:04:47 -0300

    3. Tentando utilizar o Strategy

--- DesignPatterns/src/br/com/alura/desconto/CalculadoraDeDescontos.java
+++ DesignPatterns/src/br/com/alura/desconto/CalculadoraDeDescontos.java
@@ -10,6 +10,8 @@ public class CalculadoraDeDescontos {
 		
 		
 		//toda nova regra de descontos, usar um novo if
+		//nao sabemos qual desconto aplicar. Obrigatório passar por cada tipo de desconto para ver sua aplicabilidade
+		//não indicado usar Strategy: não tem um parametro
 		if(orcamento.getQuantidadeDeItens() > 5) {
 			return orcamento.getValor().multiply(new BigDecimal("0.1"));	
 		}
