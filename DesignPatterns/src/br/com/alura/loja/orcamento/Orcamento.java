7ad24c74a7d1bb73ee6f65ce6e44daa994ae4c4a
Jefferson <jeffrm@gmail.com>
2022-06-13 23:03:57 -0300

    4. STATE
    02. Adicionando a situação do orçamento

--- DesignPatterns/src/br/com/alura/loja/orcamento/Orcamento.java
+++ DesignPatterns/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -6,6 +6,7 @@ public class Orcamento {
 
 	private BigDecimal valor;
 	private int quantidadeDeItens;
+	private String situacao;
 
 	public Orcamento(BigDecimal valor, int quantidadeDeItens) {
 		this.valor = valor;
@@ -20,6 +21,22 @@ public class Orcamento {
 
 	public int getQuantidadeDeItens() {
 		return quantidadeDeItens;
+	}
+	
+	public void aplicarDescontoExtra() {
+		BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+		
+		if(situacao.equals("EM_ANALISE")) {
+			valorDoDescontoExtra = new BigDecimal("0.05");
+		}else if(situacao.equals("APROVADO")) {
+			valorDoDescontoExtra = new BigDecimal("0.02");
+		} 
+				
+		this.valor = this.valor.subtract(valorDoDescontoExtra);	
+	}
+	
+	public void aprovar() {
+		this.situacao = "APROVADO";
 	}
 	
 	
