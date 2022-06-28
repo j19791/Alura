d13713c5d3dfa7657076417ee68e4c99482a0816
Jefferson <jeffrm@gmail.com>
2022-06-27 21:08:54 -0300

    1.6 Inferência de variáveis

--- novidades-java-minors/src/br/com/alura/inferenciavariavel/Teste.java
+++ novidades-java-minors/src/br/com/alura/inferenciavariavel/Teste.java
@@ -4,10 +4,17 @@ import java.util.HashMap;
 
 public class Teste {
 
+	//var nome = "jo√O"; VARIAVEL DE INSTANCIA N√O PODE FAZER INFERENCIA DE VARIAVEIS
+	
 	public static void main(String[] args) {
 		HashMap<String, String> cpfPorNomes = new HashMap<String, String>();
 		cpfPorNomes.put("Jo„o", "04813189");
 
 		System.out.println(cpfPorNomes);
+		
+		//jAVA 10 - INFERENCIA DE VARIAVEIS
+		var cpfPorNomes2 = new HashMap<String, String>(); //declarar os tipos q eu quero no new
+		
+		var nome = "jo√O";
 	}
 }
