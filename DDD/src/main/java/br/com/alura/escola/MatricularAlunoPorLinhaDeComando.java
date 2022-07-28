1cceaa86f637c95c57fb094f1a0e585b54db0108
Jefferson <jeffrm@gmail.com>
2022-07-27 23:09:03 -0300

    5.6 - Utilizacao dos Contextos

--- DDD/src/main/java/br/com/alura/escola/MatricularAlunoPorLinhaDeComando.java
+++ DDD/src/main/java/br/com/alura/escola/MatricularAlunoPorLinhaDeComando.java
@@ -1,11 +1,14 @@
-package br.com.alura.escola.academico;
+package br.com.alura.escola;
 
 import br.com.alura.escola.academico.aplicacao.aluno.matricular.MatricularAluno;
 import br.com.alura.escola.academico.aplicacao.aluno.matricular.MatricularAlunoDto;
 import br.com.alura.escola.academico.dominio.aluno.LogDeAlunoMatriculado;
 import br.com.alura.escola.academico.infra.aluno.RepositorioDeAlunosEmMemoria;
+import br.com.alura.escola.gamificacao.aplicacao.GeraSeloAlunoNovato;
+import br.com.alura.escola.gamificacao.infra.selo.RepositorioDeSelosEmMemoria;
 import br.com.alura.escola.shared.dominio.evento.PublicadorDeEventos;
 
+//ponto de entrada - ultiza multiplos contextos
 public class MatricularAlunoPorLinhaDeComando {
 	
 	public static void main(String[] args) {
@@ -15,6 +18,7 @@ public class MatricularAlunoPorLinhaDeComando {
 		
 		PublicadorDeEventos publicador = new PublicadorDeEventos();
 		publicador.adicionar(new LogDeAlunoMatriculado());
+		publicador.adicionar(new GeraSeloAlunoNovato(new RepositorioDeSelosEmMemoria()));
 		
 		MatricularAluno matricular = new MatricularAluno(
 				new RepositorioDeAlunosEmMemoria(),
