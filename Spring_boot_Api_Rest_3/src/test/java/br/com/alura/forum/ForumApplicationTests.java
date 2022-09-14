0981402679d1593fae0e13231756c764edbe3b85
Jefferson <jeffrm@gmail.com>
2022-09-13 22:14:46 -0300

    3. Testes Automatizados
    3-4: Testando um repository

--- Spring_boot_Api_Rest_3/src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ Spring_boot_Api_Rest_3/src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -1,16 +1,18 @@
 package br.com.alura.forum;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest //Spring inicialize o servidor e carregue os beans da aplicação durante a execução dos testes automatizados.
 public class ForumApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertTrue(true);
 	}
 
 }
