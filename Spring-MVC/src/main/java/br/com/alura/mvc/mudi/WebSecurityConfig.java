4ed1ebcf2264800724d023d26da50d9b0cf24960
Jefferson <jeffrm@gmail.com>
2022-09-27 12:03:37 -0300

    2- Provedor de autenticação
    2.2- Usando JDBC Authentication

--- Spring-MVC/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ Spring-MVC/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,18 +1,28 @@
 package br.com.alura.mvc.mudi;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private DataSource dataSource;
+
 
 	@Override
 	//configuracao de autorizacao
@@ -21,29 +31,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 			.anyRequest().authenticated() //para todas as requisições o usuário deverá estar autenticado
 			.and()
 			.formLogin(form -> form.loginPage("/login")
+				.defaultSuccessUrl("/home", true)
 				.permitAll()
 			)
 			.logout(logout -> logout.logoutUrl("/logout"));
 	}
 	
-	
-	@Bean
 	@Override
-	public UserDetailsService userDetailsService() {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
-		UserDetails user = 
-			User
-				.withDefaultPasswordEncoder() //nao usar em producao
-				.username("joao")
-				.password("joao")
-				.roles("ADM")
-				.build();
+		/*
+		 * UserDetails user = User .builder() .username("maria")
+		 * .password(encoder.encode("maria")) .roles("ADM") .build();
+		 */
 		
-		return new InMemoryUserDetailsManager(user);
-		
-		
-		
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(encoder);
+			/*.withUser(user);*/
 	}
+	
+	
+
 	
 	
 }
