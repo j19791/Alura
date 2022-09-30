e2715fe29fa0b3594ccef2e39579d63d73bc3131
Jefferson <jeffrm@gmail.com>
2022-09-30 11:11:22 -0300

    3.4- Trabalhando com paginação

--- Spring-MVC/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ Spring-MVC/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -4,11 +4,11 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
@@ -25,7 +25,11 @@ public class HomeController {
 	@GetMapping()
 	public String home(Model model, 
 			Principal principal) {//Principal: dados do usuário logado usando Spring Security 
-		List<Pedido> pedidos = repository.findByStatus(StatusPedido.ENTREGUE);
+		
+		Sort sort = Sort.by("dataDaEntrega").descending();
+		PageRequest paginacao = PageRequest.of(0, 1, sort); //paginacao e ordenacao: a partir da pagina 0, 1 itens por pagina, descendente de data de entrega
+		
+		List<Pedido> pedidos = repository.findByStatus(StatusPedido.ENTREGUE, paginacao);
 		model.addAttribute("pedidos", pedidos);
 		return "home"; 
 	}
