7482e2b5f626e34a02861c00c11a91fdc9946ba9
Jefferson <jeffrm@gmail.com>
2022-10-03 12:32:29 -0300

    5.7 - Guardando ofertas

--- Spring-MVC/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ Spring-MVC/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -2,7 +2,9 @@ package br.com.alura.mvc.mudi.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -11,6 +13,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Pedido {
@@ -30,9 +35,15 @@ public class Pedido {
 	
 	//Muitos pedidos para um usuário
 	@ManyToOne(fetch = FetchType.LAZY)//lazy: listar só pedidos sem listar dados do usuário
+	@JsonIgnore  //nao me lembro disso: colocando para parar os erros de hibernateLazyInitializer ???
 	private User user;
 	
-
+	@OneToMany(cascade = CascadeType.ALL,
+			mappedBy = "pedido",
+			fetch = FetchType.LAZY) //quando eu buscar um pedido, ele não vai automaticamente carregar também no banco as ofertas.
+	private List<Oferta> ofertas; 
+	
+	
 	public String getNomeProduto() {
 		return nomeProduto;
 	}
@@ -95,6 +106,22 @@ public class Pedido {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<Oferta> getOfertas() {
+		return ofertas;
+	}
+
+	public void setOfertas(List<Oferta> ofertas) {
+		this.ofertas = ofertas;
 	}
 
 	
