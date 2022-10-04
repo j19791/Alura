a6afc38ae16f4e91fb84c5feebf0db66a2d31ff2
Jefferson <jeffrm@gmail.com>
2022-10-04 12:10:07 -0300

    6.4- Aplicando validadores com Bean Vallidators

--- Spring-MVC/src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovaOferta.java
+++ Spring-MVC/src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovaOferta.java
@@ -4,16 +4,24 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import br.com.alura.mvc.mudi.model.Oferta;
 
 public class RequisicaoNovaOferta {
 
 	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
+	
 	private Long pedidoId;
 	
+	@Pattern(regexp = "^\\d+(\\.\\d+{2})?$")//começar com digito, varios digitos. Depois um ponto e  depois mais 2 digitos (opcionais)
+	@NotNull
 	private String valor;
 	
+	@Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}$")
+	@NotNull
 	private String dataDaEntrega;
 	
 	private String comentario;
